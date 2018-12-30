0be751b07ed3638172f49f2274b9f9243fbb348c
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-30 23:50:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,23 +1,25 @@
-contacts = {
-  "Jon Snow" => {
-    name: "Jon",
-    email: "jon_snow@thewall.we",
-    favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-    knows: nil
-  },
-  "Freddy Mercury" => {
-    name: "Freddy",
-    email: "freddy@mercury.com",
-    favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  }
-}
+require 'pry'
 
-def first_challenge(contacts)
-contacts.each do |person, data|
-  data.each do |attribute, value|
-    if attribute == :favorite_icecream_flavors
-      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+  # This is the array we will be passing into the remove_strawberry method
+  # contacts = {
+  #   "Jon Snow" => {
+  #     name: "Jon",
+  #     email: "jon_snow@thewall.we",
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #   },
+  #   "Freddy Mercury" => {
+  #     name: "Freddy",
+  #     email: "freddy@mercury.com",
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #   }
+  # }
+
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if{|ice_cream| ice_cream == "strawberry"}
+      end
     end
-  #remember to return your newly altered contacts hash!
-  contacts
+  end
 end
