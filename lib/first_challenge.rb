531176555f3cffcb9e14c96fc6fc1e5dbc90d4d6
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-30 23:47:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete_if {ice_cream| ice_cream == "strawberry"}
-
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
