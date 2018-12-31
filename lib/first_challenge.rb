6beccee2bb38d1014701e911eecadb4933486187
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-31 00:02:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,16 +13,14 @@ def first_challenge
     }
   }
   #your code here
-  contacts.each do |person, data|
-       data.each do |key, value|
-          if key == :favorite_icecream_flavors
-              value.delete_if do |flavor|
-              flavor == "strawberry"
-          end
-      end
-  end
-  #remember to return your newly altered contacts hash!	   #remember to return your newly altered contacts hash!
-  contacts	   contacts
-end	 end
-
- end
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+end
+end
+end
+  #remember to return your newly altered contacts hash!
+  contacts
+end
+end
